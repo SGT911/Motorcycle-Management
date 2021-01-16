c62e482ae283737e69db2072e261ee163877e954
Sebastian Gaviria Tangarife <5861675-SGT911@users.noreply.gitlab.com>
2021-01-16 22:01:53 +0000

    FIX: resources_details view

--- database/01-views.sql
+++ database/01-views.sql
@@ -6,11 +6,11 @@ CREATE VIEW resources_details
 	AS SELECT
 		res.id,
 		res.resource_date,
-		GROUP_CONCAT(',', usr.user_name) AS users,
+		GROUP_CONCAT(usr.user_name SEPARATOR ',') AS users,
 		COUNT(used_res.id) AS user_count
 	FROM resources AS res
-		INNER JOIN used_resources AS used_res
+		LEFT JOIN used_resources AS used_res
 			ON res.id = used_res.resource
-		INNER JOIN users AS usr
-			ON usr.id = used_res.user
+		inner JOIN users AS usr
+			ON used_res.user = usr.id
 	GROUP BY res.id, res.resource_date;
