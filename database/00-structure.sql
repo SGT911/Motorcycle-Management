777f72d804def072a3f077fdddf562ad4f3ac383
Sebastian Gaviria Tangarife <5861675-SGT911@users.noreply.gitlab.com>
2021-01-16 16:56:42 +0000

    FIX: Minor errors sql DB

--- database/00-structure.sql
+++ database/00-structure.sql
@@ -2,7 +2,7 @@
 -- Structure --
 ---------------
 
-CREATE TABLE user (
+CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	user_name VARCHAR(16) NOT NULL UNIQUE,
 	password VARCHAR(32) NOT NULL,
@@ -17,8 +17,11 @@ CREATE TABLE resources (
 CREATE TABLE used_resources (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	resource INT NOT NULL,
-	user INT NOT NULL,
-
-	FOREIGN KEY (resource) REFERENCES resources(id),
-	FOREIGN KEY (user) REFERENCES users(id)
+	user INT NOT NULL
 );
+
+ALTER TABLE used_resources ADD
+	FOREIGN KEY (resource) REFERENCES resources(id);
+
+ALTER TABLE used_resources ADD
+	FOREIGN KEY (user) REFERENCES users(id);
