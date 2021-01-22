e78f05f873a3fd5402dbab3e2a535e3aceead435
Sebastian Gaviria Tangarife <5861675-SGT911@users.noreply.gitlab.com>
2021-01-22 16:22:52 +0000

    FIX all :"(

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-source $(dirname $0)/venv/bin/activate
+cd $(dirname $0)
+
+source ./venv/bin/activate
 gunicorn -k flask_sockets.worker -w 1 -b unix:/tmp/motorcycle_management.sock wsgi:app
 deactivate
